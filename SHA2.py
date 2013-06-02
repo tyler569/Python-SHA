f5aa44cb5575282e3629eba4eecb3cafef3bd6c6
Tyler P <t5j6p9@gmail.com>
2013-06-02 16:50:36 -0400

    Added support for all other SHA2 hash functions.
    Default command syntax -
    $ python SHA2.py [bits:(224, 256, 384, 512, 512/224, 512/256)] [message]
    
    Simplified digest reporting, changed digest to return int from hex-based str.
    
    Improvements to comments and added _ to protected funcs.
    
    (Multiple commits squashed)

--- SHA2.py
+++ SHA2.py
@@ -1,6 +1,7 @@
 import sys
 
-def init_vars(bits):
+
+def _init_vars(bits, sh):
     '''
     Initialises variables for the SHA256 hash algorithm.
 
@@ -8,44 +9,129 @@ def init_vars(bits):
 
     Returns tuple - (hash_vars, ROUND_CONSTS)
     '''
-    if bits == 224:
-        hash_vars = [
-            0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939,
-            0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4
+    if bits in (224, 256):
+        if bits == 224:
+            hash_vars = [
+                0xc1059ed8, 0x367cd507, 0x3070dd17, 0xf70e5939,
+                0xffc00b31, 0x68581511, 0x64f98fa7, 0xbefa4fa4
+            ]
+        elif bits == 256:
+            hash_vars = [
+                0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+                0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+            ]
+        ROUND_CONSTS = [
+            0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+            0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+            0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+            0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+            0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+            0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+            0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+            0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+            0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+            0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+            0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+            0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+            0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+            0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+            0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+            0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
         ]
-    elif bits == 256:
-        hash_vars = [
-            0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-            0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+        ROT_COUNTS = [
+            [2, 13, 22],
+            [6, 11, 25],
+            [7, 18, 3],
+            [17, 19, 10]
         ]
-    ROUND_CONSTS = [
-        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
-        0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-        0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
-        0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
-        0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-        0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-    ]
-    return hash_vars, ROUND_CONSTS
+    elif bits in (384, 512):
+        if bits == 384:
+            hash_vars = [
+                0xcbbb9d5dc1059ed8, 0x629a292a367cd507,
+                0x9159015a3070dd17, 0x152fecd8f70e5939,
+                0x67332667ffc00b31, 0x8eb44a8768581511,
+                0xdb0c2e0d64f98fa7, 0x47b5481dbefa4fa4
+            ]
+        elif bits == 512:
+            hash_vars = [
+                0x6a09e667f3bcc908, 0xbb67ae8584caa73b,
+                0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
+                0x510e527fade682d1, 0x9b05688c2b3e6c1f,
+                0x1f83d9abfb41bd6b, 0x5be0cd19137e2179
+            ]
+        ROUND_CONSTS = [
+            0x428a2f98d728ae22, 0x7137449123ef65cd,
+            0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
+            0x3956c25bf348b538, 0x59f111f1b605d019,
+            0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
+            0xd807aa98a3030242, 0x12835b0145706fbe,
+            0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
+            0x72be5d74f27b896f, 0x80deb1fe3b1696b1,
+            0x9bdc06a725c71235, 0xc19bf174cf692694,
+            0xe49b69c19ef14ad2, 0xefbe4786384f25e3,
+            0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
+            0x2de92c6f592b0275, 0x4a7484aa6ea6e483,
+            0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
+            0x983e5152ee66dfab, 0xa831c66d2db43210,
+            0xb00327c898fb213f, 0xbf597fc7beef0ee4,
+            0xc6e00bf33da88fc2, 0xd5a79147930aa725,
+            0x06ca6351e003826f, 0x142929670a0e6e70,
+            0x27b70a8546d22ffc, 0x2e1b21385c26c926,
+            0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
+            0x650a73548baf63de, 0x766a0abb3c77b2a8,
+            0x81c2c92e47edaee6, 0x92722c851482353b,
+            0xa2bfe8a14cf10364, 0xa81a664bbc423001,
+            0xc24b8b70d0f89791, 0xc76c51a30654be30,
+            0xd192e819d6ef5218, 0xd69906245565a910,
+            0xf40e35855771202a, 0x106aa07032bbd1b8,
+            0x19a4c116b8d2d0c8, 0x1e376c085141ab53,
+            0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8,
+            0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb,
+            0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3,
+            0x748f82ee5defb2fc, 0x78a5636f43172f60,
+            0x84c87814a1f0ab72, 0x8cc702081a6439ec,
+            0x90befffa23631e28, 0xa4506cebde82bde9,
+            0xbef9a3f7b2c67915, 0xc67178f2e372532b,
+            0xca273eceea26619c, 0xd186b8c721c0c207,
+            0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178,
+            0x06f067aa72176fba, 0x0a637dc5a2c898a6,
+            0x113f9804bef90dae, 0x1b710b35131c471b,
+            0x28db77f523047d84, 0x32caab7b40c72493,
+            0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c,
+            0x4cc5d4becb3e42b6, 0x597f299cfc657e2a,
+            0x5fcb6fab3ad6faec, 0x6c44198c4a475817
+        ]
+        ROT_COUNTS = [
+            [28, 34, 39],
+            [14, 18, 41],
+            [1, 8, 7],
+            [19, 61, 6]
+        ]
+    if 512 - sh == 224:    #512/224
+        hash_vars = [
+            0x8C3D37C819544DA2, 0x73E1996689DCD4D6,
+            0x1DFAB7AE32FF9C82, 0x679DD514582F9FCF,
+            0x0F6D2B697BD44DA8, 0x77E36F7304C48942,
+            0x3F9D85A86A1D36C8, 0x1112E6AD91D692A1
+        ]
+    elif 512 - sh == 256:  #512/256
+        hash_vars = [
+            0x22312194FC2BF72C, 0x9F555FA3C84C64C2,
+            0x2393B86B6F53B151, 0x963877195940EABD,
+            0x96283EE2A88EFFE3, 0xBE5E1E2553863992,
+            0x2B0199FC2C85B8AA, 0x0EB72DDC81C52CA2
+        ]
+    return hash_vars, ROUND_CONSTS, ROT_COUNTS
     
-def msg2chunks(message):
+def _msg2chunks(message, bits):
     '''
-    Converts command line input to SHA256 chunks.
+    Converts message input to SHA2 chunks.
+    
+    Takes message as str and bits as selection of function, eg:
+        224  means for SHA224
+        256  means for SHA256
 
-    Takes command line (concatinates all arguments [1:] with spaces
-    to mirror how they are input.  Can be given all as one argument as well
-
-    Returns array of SHA256 chunks corresponding to input data
+    Returns array of SHA2 chunks corresponding to input data and function
     as binary string
     '''
     message_bin = ''
@@ -55,16 +141,23 @@ def msg2chunks(message):
     length = len(message_bin)
     message_bin += '1' # Appended bit
     chunks = []
-
-    pad_0s = '0' * (512 - (len(message_bin) + 64) % 512)
+    
+    if bits in (224, 256):
+        chunklen = 512
+        lenlen = 64
+    elif bits in (384, 512):
+        chunklen = 1024
+        lenlen = 128
+    
+    pad_0s = '0' * (chunklen - (len(message_bin) + lenlen) % chunklen)
     chunk_bin = message_bin + pad_0s
-    length_bin = fill_0s(bin(length)[2:], 64)
+    length_bin = fill_0s(bin(length)[2:], lenlen)
     chunk_bin += length_bin
     chunks = []
     
-    if len(chunk_bin) > 512:
-        for i in range(int(len(chunk_bin) / 512)):
-            chunks.append(chunk_bin[512*i:512*(i+1)])
+    if len(chunk_bin) > chunklen:
+        for i in range(int(len(chunk_bin) / chunklen)):
+            chunks.append(chunk_bin[chunklen*i:chunklen*(i+1)])
     else:
         chunks = [chunk_bin]
     return chunks
@@ -78,29 +171,30 @@ def fill_0s(val, places):
     Returns same number but with n 0's added such that len(input + n*'0')
     is == places.  Function also snips '0b' from out
     '''
-    out = (places - len(val)) * '0' + val
+    out = ((places - len(val)) * '0') + val
     return out
 	
-def words(chunk_bin):
+def _words(chunk_bin, ct, mod, C):
     '''
-    Given an SHA256 chunk, function generated the word array corresponding to it
+    Given an SHA chunk and information about what function is being called,
+    function generated the word array corresponding to it
 
     Takes SHA256 chunk as binary string
 
-    Returns array of 64 binary words as strings for use in SHA256 calculation
+    Returns array of (ct) binary words as strings for use in SHA calculation
     '''
-    w = [0 for i in range(64)]
+    w = [0 for i in range(ct)]
     for i in range(16):
-        start = 32*i
-        end = 32*(i+1)
+        start = mod*i
+        end = mod*(i+1)
         w[i] = int(chunk_bin[start:end], 2)
-    for i in range(16, 64):
-        s0 = ror(w[i-15], 7) ^ ror(w[i-15], 18) ^ (w[i-15] >> 3)
-        s1 = ror(w[i-2], 17) ^ ror(w[i-2], 19) ^ (w[i-2] >> 10)
-        w[i] = (w[i-16] + s0 + w[i-7] + s1) % 2**32
+    for i in range(16, ct):
+        s0 = ror(w[i-15], C[2][0], mod)^ror(w[i-15], C[2][1], mod)^(w[i-15] >> C[2][2])
+        s1 = ror(w[i-2], C[3][0], mod)^ror(w[i-2], C[3][1], mod)^(w[i-2] >> C[3][2])
+        w[i] = (w[i-16] + s0 + w[i-7] + s1) % 2**mod
     return w
 	
-def ror(num, val):
+def ror(num, val, mod):
     '''
     Binary right rotate
 
@@ -108,54 +202,62 @@ def ror(num, val):
 
     Returns int - rotated number
     '''
-    pre = (num % 2 ** val) * 2**(32 - val) 
+    pre = (num % 2 ** val) * 2**(mod - val) 
     post = num >> val
     return pre + post
 	
-def sha2hash(message, bits):
+def sha2hash(message, bits, sh):
     '''
-    Main loop of the SHA256 hash algorithm
+    Main loop of the SHA2 hash algorithm
 
-    Takes data and digest bits as input, dies if bits not in (224, 256)
+    Takes data and digest bits as input, dies if (bits) not in SHA2
 
-    Returns hash digest as hex string without '0x'
+    Returns hash digest as int
     '''
-    if not bits in (224, 256):
-        print('Only SHA224 and SHA256 are supported')
+    if not bits in (224, 256, 384, 512):
+        print('Only standard SHA2 functions are supported')
         sys.exit()
-    chunks = msg2chunks(message)
-    h, k = init_vars(bits)
+    if bits in (224, 256):
+        rounds = 64
+        mod = 32
+    elif bits in (384, 512):
+        rounds = 80
+        mod = 64
+    chunks = _msg2chunks(message, bits)
+    h, k, C = _init_vars(bits, sh)
     for chunk in chunks:
         xs = h[:]
-        w = words(chunk)
-        for i in range(64):
-            S1 = ror(xs[4], 6) ^ ror(xs[4], 11) ^ ror(xs[4], 25)
-            ch = (xs[4] & xs[5]) ^ (~xs[4] & xs[6])
-            temp = (xs[7] + S1 + ch + k[i] + w[i]) % 2**32
-            xs[3] = (xs[3] + temp) % 2**32
-            S0 = ror(xs[0], 2) ^ ror(xs[0], 13) ^ ror(xs[0], 22)
-            maj = (xs[0] & xs[1]) ^ (xs[0] & xs[2]) ^ (xs[1] & xs[2])
-            temp = (temp + S0 + maj) % 2**32
+        w = _words(chunk, rounds, mod, C)
+        for i in range(rounds):
+            S1 = ror(xs[4], C[1][0], mod)^ror(xs[4], C[1][1], mod)^ror(xs[4], C[1][2], mod)
+            ch = (xs[4] & xs[5])^(~xs[4] & xs[6])
+            temp = (xs[7] + S1 + ch + k[i] + w[i]) % 2**mod
+            xs[3] = (xs[3] + temp) % 2**mod
+            S0 = ror(xs[0], C[0][0], mod)^ror(xs[0], C[0][1], mod)^ror(xs[0], C[0][2], mod)
+            maj = (xs[0] & xs[1])^(xs[0] & xs[2])^(xs[1] & xs[2])
+            temp = (temp + S0 + maj) % 2**mod
             
             xs = [temp] + [xs[i-1] for i in range(1, 8)]
-            # print(str(i) + ': ' + ' '.join([hex(int(i))[2:] for i in xs]))
-        h = [(h[i] + xs[i]) % 2**32 for i in range(8)]
-    if bits == 256:
-        digest = ''.join([fill_0s(hex(i)[2:],8) for i in h])
-    elif bits == 224:
-        digest = ''.join([fill_0s(hex(i)[2:],8) for i in h[:7]])
+            # print(str(i) + ': ' + ' '.join([hex(int(i))[2:] for i in xs]) + '\n') ##
+        h = [(h[i] + xs[i]) % 2**mod for i in range(8)]
+    digest = sum([h[i] << mod * (7-i) for i in range(8)]) >> sh
     return(digest)
 
     
 def main():
-    bits = int(sys.argv[1])
+    bits = sys.argv[1]
+    if bits in ('512/224', '512/256'):
+        sh = 512 - int(bits[4:])
+        bits = 512
+    else:
+        bits = int(bits)
+        sh = {224:32, 256:0, 384:128, 512:0}[bits]
     try:
         message = sys.argv[2]
     except IndexError:
         message = ''
-    digest = sha2hash(message, bits)
-    digest = '0x' + digest#.upper()
-    print(digest)
+    digest = sha2hash(message, bits, sh)
+    print(hex(digest))
     
     
 if __name__ == '__main__':
